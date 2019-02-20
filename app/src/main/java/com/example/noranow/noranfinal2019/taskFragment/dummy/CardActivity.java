3d781ae06bac982c6c796f7e93cb25ed2ac10e70
Owner <noran@danon.tzafonet.org.il>
2019-02-20 08:53:11 +0200

    aaaa

--- app/src/main/java/com/example/noranow/noranfinal2019/taskFragment/dummy/CardActivity.java
+++ app/src/main/java/com/example/noranow/noranfinal2019/taskFragment/dummy/CardActivity.java
@@ -2,10 +2,13 @@ package com.example.noranow.noranfinal2019.taskFragment.dummy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.noranow.noranfinal2019.R;
 
-public class Card extends AppCompatActivity {
+public class CardActivity extends AppCompatActivity {
+    private TextView tvcard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
