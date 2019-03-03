4d55db38251151f7c3e99f9b2592717a82a02af5
Owner <noran@danon.tzafonet.org.il>
2019-03-03 12:21:40 +0200

    mjgjhg

--- app/src/main/java/com/example/noranow/noranfinal2019/ShowAllTasksActivity.java
+++ app/src/main/java/com/example/noranow/noranfinal2019/ShowAllTasksActivity.java
@@ -4,15 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.example.noranow.noranfinal2019.data.MyTask;
 import com.example.noranow.noranfinal2019.data.TaskAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 
 public class ShowAllTasksActivity extends AppCompatActivity {
-    private  ListView lvTasks;
+    private  ListView lsvTasks;
     private  TaskAdapter taskAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_all_tasks);
+        lsvTasks=findViewById(R.id.lstvTask);
+        taskAdapter=new TaskAdapter(getBaseContext(),R.layout.task_item);
+        lsvTasks.setAdapter(taskAdapter);
+        readTask();
+    }
+    private void readTask(){
+        final ArrayList<MyTask>myTasks=new ArrayList<>();
+        // reference to the database root
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+         databaseReference.child("MyTasks").addChildEventListener(new ValueEventListener(){
+
+
     }
 }
